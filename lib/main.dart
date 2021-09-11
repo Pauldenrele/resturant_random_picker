7a2d1f8ebd8f1281b65a4790200471908c32f79a
PaulDenrele <pauldenrele@gmail.com>
2021-09-11 08:21:37 +0100

    Refactored random_restaurant.dart to another file

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'dart:math';
+import 'package:resturant_random_picker/random_restaurant.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,58 +8,6 @@ void main() {
 class MyApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    return _MyAppState();
-  }
-}
-
-class _MyAppState extends State<MyApp> {
-  List<String> restaurants = [
-    'McDonald\'s',
-    'Roscoe\'s Chicken & Waffles',
-    'Olive Garden',
-    'Pizza Hut',
-    'Panda Express',
-    'Subway'
-  ];
-
-  int? currentIndex;
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-          body: Center(
-              child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text('What do you want to eat?'),
-          if (currentIndex != null)
-            Text(
-              restaurants[currentIndex!],
-              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-              textAlign: TextAlign.center,
-            ),
-          Padding(
-            padding: EdgeInsets.only(top: 50),
-          ),
-          FlatButton(
-            onPressed: () {
-              updateIndex();
-            },
-            child: Text('Pick Restaurant'),
-            color: Colors.purple,
-            textColor: Colors.white,
-          )
-        ],
-      ))),
-    );
-  }
-
-  void updateIndex() {
-    final random = Random();
-    final index = random.nextInt(restaurants.length);
-    setState(() {
-      currentIndex = index;
-    });
+    return restaurantState();
   }
 }
---
+++ lib/random_restaurant.dart
@@ -0,0 +1,101 @@
+import 'dart:math';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'main.dart';
+
+class restaurantState extends State<MyApp> {
+
+  List<String> restaurants = [
+
+    'McDonald\'s',
+
+    'Roscoe\'s Chicken & Waffles',
+
+    'Olive Garden',
+
+    'Pizza Hut',
+
+    'Panda Express',
+
+    'Subway'
+
+  ];
+
+
+  int? currentIndex;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+      home: Scaffold(
+
+          body: Center(
+
+              child: FittedBox(
+
+                child: Column(
+
+                  mainAxisAlignment: MainAxisAlignment.center,
+
+                  children: [
+
+                    Text('What do you want to eat?'),
+
+                    if (currentIndex != null)
+
+                      Text(
+
+                        restaurants[currentIndex!],
+
+                        style: TextStyle(
+                            fontSize: 30, fontWeight: FontWeight.bold),
+
+                        textAlign: TextAlign.center,
+
+                      ),
+
+                    Padding(
+
+                      padding: EdgeInsets.only(top: 50),
+
+                    ),
+
+                    FlatButton(
+
+                      onPressed: () {
+                        updateIndex();
+                      },
+
+                      child: Text('Pick Restaurant'),
+
+                      color: Colors.purple,
+
+                      textColor: Colors.white,
+
+                    )
+
+                  ],
+
+                ),
+
+              ))),
+
+    );
+  }
+
+
+  void updateIndex() {
+    final random = Random();
+
+    final index = random.nextInt(restaurants.length);
+
+    setState(() {
+      currentIndex = index;
+    });
+  }
+
+}
